e1a70ca48069bd7815f59c92860e82d9a20db339
Nikita Polovinkin <poloniki@ya.ru>
2023-02-11 07:43:40 +0100

    Fix

--- pages/Explore.py
+++ pages/Explore.py
@@ -12,4 +12,5 @@ df = pd.read_sql_query("SELECT * FROM workshop", conn)
 df = df.drop(columns=['cuisine'])
 df = df.drop(columns=['thezos'])
 df.columns = ['Name', 'Twitter',  'Teia']
+st.subheader('Follow your peers on Twitter and check their NFTs on Teia. Build your network and get inspired!')
 st.write(df.style.format({'Twitter': '<a href="{}" target="_blank">Twitter</a>', 'Tezos': '<a href="{}" target="_blank">Tezos</a>', 'Teia': '<a href="{}" target="_blank">NFT</a>'}).render(), unsafe_allow_html=True)
